411f761fbe43661b4ed96f64f032b545710db514
kmsheng <kmsh3ng@gmail.com>
2017-03-22 19:28:51 +0800

    add class ipc

---
+++ src/helpers/ipc.js
@@ -0,0 +1,24 @@
+const ipcRenderer = window.require('electron').ipcRenderer;
+const crypto = window.require('crypto');
+
+export default class ipc {
+
+  static events = [];
+
+  static send(name, data = {}) {
+    return new Promise((resolve, reject) => {
+      const id = crypto.randomBytes(48).toString('hex');
+      data._id = id;
+      ipcRenderer.send(name, data);
+      ipcRenderer.once(`${name}::${id}`, (event, data) => {
+        return data.error ? reject(data) : resolve(data);
+      });
+    });
+  }
+
+  static on(name, cb) {
+  }
+
+  static off(name) {
+  }
+}
