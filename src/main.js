d3eaa9bf6b1f40b548bdbdcaeae0d82c3551d212
kmsheng <kmsh3ng@gmail.com>
2017-03-22 19:30:38 +0800

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import {app, screen, BrowserWindow} from 'electron';
+import {app, screen, BrowserWindow, ipcMain} from 'electron';
+import path from 'path';
+
+import {mkdirp, ipcDecorator} from './main/helpers';
+import {APP_DATA_PATH} from './main/constants';
+
+import * as folder from './main/controllers/folder';
+
+const sqlite3 = require('sqlite3').verbose();
+const dbPath = path.resolve(APP_DATA_PATH, 'garchen.sqlite3');
+const db = new sqlite3.Database(dbPath);
+
+const ipc = ipcDecorator.decorate(ipcMain);
 
 let mainWindow = null;
 
@@ -20,3 +32,5 @@ app.on('ready', () => {
     mainWindow = null;
   });
 });
+
+ipc.on('GET /folders', folder.listFolders);
