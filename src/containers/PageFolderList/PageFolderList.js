df54db0753660fe99a174a4aad9f6094f01f3e22
kmsheng <kmsh3ng@gmail.com>
2017-03-23 11:38:39 +0800

    update container PageFolderList

--- src/containers/PageFolderList/PageFolderList.js
+++ src/containers/PageFolderList/PageFolderList.js
@@ -1,14 +1,38 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
+import classNames from 'classnames';
+import {Button} from 'react-bootstrap';
+
+import {loadFolders} from './../../redux/modules/folder';
+import {mapConnect} from './../../helpers';
 
 const styles = require('./PageFolderList.scss');
 
+@connect((state) => mapConnect(state, {
+  folders: 'folder.folders'
+}), {loadFolders})
 export default class PageFolderList extends Component {
+
+  static propTypes = {
+    folders: PropTypes.array.isRequired,
+    loadFolders: PropTypes.func.isRequired
+  };
+
+  componentWillMount() {
+    this.props.loadFolders();
+  }
 
   render() {
 
     return (
-      <div className={styles.pageFolderList}>
-        <h1>Page Folder List</h1>
+      <div className={classNames('container', 'list', styles.pageFolderList)}>
+        <div className="topbar">
+          <h1>Folders</h1>
+          <Button className="btn-add">
+            <i className="fa fa-plus" />
+            <span>Add Folder</span>
+          </Button>
+        </div>
       </div>
     );
   }
