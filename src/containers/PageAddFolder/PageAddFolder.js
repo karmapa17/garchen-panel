ba5ee4b739de0739876145e2da669d8665d9ff0b
kmsheng <kmsh3ng@gmail.com>
2017-03-26 11:32:25 +0800

    add container PageAddFolder

---
+++ src/containers/PageAddFolder/PageAddFolder.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+const styles = require('./PageAddFolder.scss');
+
+export default class PageAddFolder extends Component {
+
+  render() {
+    return (
+      <div className={styles.pageAddFolder}>
+        <h1>Add Folder</h1>
+      </div>
+    );
+  }
+}
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,4 +1,5 @@
 export App from './App/App';
 export PageAbout from './PageAbout/PageAbout';
+export PageAddFolder from './PageAddFolder/PageAddFolder';
 export PageFolderList from './PageFolderList/PageFolderList';
 export PageHome from './PageHome/PageHome';
