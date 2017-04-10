e8bc51157443a3aff401989030b78ec96b725f4e
kmsheng <kmsh3ng@gmail.com>
2017-04-10 23:32:01 +0800

    update

--- bin/build.sh
+++ bin/build.sh
@@ -7,9 +7,12 @@ ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 ZIP_IOS="${PACKAGE_NAME}-darwin-x64-v${PACKAGE_VERSION}.zip"
 ZIP_WIN="${PACKAGE_NAME}-ia32-v${PACKAGE_VERSION}.zip"
 
+set -e
 cd $ROOT_DIR
+rm -rf dist zips
 mkdir -p dist/src
 mkdir -p zips
+
 NODE_ENV=production webpack
 sed -i '' -e 's/http:\/\/localhost:3000\///g' dist/index.html
 mv dist/index.html dist/bundle.js dist/src/
@@ -17,13 +20,10 @@ cp -r assets/images/*.ico assets/images/*.icns .babelrc package.json index.js di
 cp -r src/main.js src/main dist/src
 
 cd dist
-npm install --verbose --production &&
+npm i --prod
+./node_modules/.bin/electron-rebuild -t prod
 
 electron-packager ./ ${PACKAGE_NAME} --platform=win32 --arch=ia32 --version="${ELECTRON_VERSION}" --app-version="${PACKAGE_VERSION}" --icon=treasure_logo.ico &&
 zip -r "${ZIP_WIN}" "${PACKAGE_NAME}-win32-ia32"
-cp ${ZIP_WIN} ./../zips/
-rm -rf "${PACKAGE_NAME}-win32-ia32*" &&
-electron-packager ./ ${PACKAGE_NAME} --platform=darwin --arch=x64 --version="${ELECTRON_VERSION}" --app-version="${PACKAGE_VERSION}" --icon=treasure_logo.icns &&
-zip -r "${ZIP_IOS}" "${PACKAGE_NAME}-darwin-x64" &&
-cp ${ZIP_IOS} ./../zips/
-rm -rf *
+mv ${ZIP_WIN} ./../zips/
+rm -rf "${PACKAGE_NAME}-win32-ia32*"
