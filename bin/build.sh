126d8c0768d81140aa97e52e56f4995503067844
kmsheng <kmsh3ng@gmail.com>
2017-04-13 14:09:57 +0800

    update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+set -x
+
 PACKAGE_NAME=$(cat package.json | grep name | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | xargs)
 ELECTRON_VERSION=1.6.1
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | xargs)
 ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 ZIP_IOS="${PACKAGE_NAME}-darwin-x64-v${PACKAGE_VERSION}.zip"
-ZIP_WIN="${PACKAGE_NAME}-ia32-v${PACKAGE_VERSION}.zip"
+ZIP_WIN="${PACKAGE_NAME}-win32-ia32-v${PACKAGE_VERSION}.zip"
 
 cd $ROOT_DIR
+rm -r dist zips
 mkdir -p dist/src
 mkdir -p zips
 NODE_ENV=production webpack
@@ -17,17 +20,14 @@ cp -r assets/images/*.ico assets/images/*.icns .babelrc package.json index.js di
 cp -r src/main.js src/main dist/src
 
 cd dist;
-npm install --verbose --production;
-
+npm install --production > /dev/null;
 
 electron-packager ./ ${PACKAGE_NAME} --platform=win32 --arch=ia32\
   --version="${ELECTRON_VERSION}" --app-version="${PACKAGE_VERSION}" --icon=treasure_logo.ico
-zip -r "${ZIP_WIN}" "${PACKAGE_NAME}-win32-ia32"
-cp ${ZIP_WIN} ./../zips/
-rm -rv "${PACKAGE_NAME}-win32-ia32*"
+zip -r "./../zips/${ZIP_WIN}" "${PACKAGE_NAME}-win32-ia32" > /dev/null
+rm -r "${PACKAGE_NAME}-win32-ia32"
 
 electron-packager ./ ${PACKAGE_NAME} --platform=darwin --arch=x64\
   --version="${ELECTRON_VERSION}" --app-version="${PACKAGE_VERSION}" --icon=treasure_logo.icns
-zip -r "${ZIP_IOS}" "${PACKAGE_NAME}-darwin-x64"
-cp ${ZIP_IOS} ./../zips/
-rm -rv "${PACKAGE_NAME}-darwin-x64*"
+zip -r "./../zips/${ZIP_IOS}" "${PACKAGE_NAME}-darwin-x64" > /dev/null
+rm -r "${PACKAGE_NAME}-darwin-x64"
