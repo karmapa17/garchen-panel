c9f9470e3966a254a2aab7a0133ff56878542c10
kmsheng <kmsh3ng@gmail.com>
2017-04-24 23:19:45 +0800

    add source to model folder

--- src/main/models/folder.js
+++ src/main/models/folder.js
@@ -3,6 +3,7 @@ module.exports = {
   schema: {
     id: {type: 'increments', primary: true},
     name: {type: String, unique: true, nullable: false},
+    source: {type: String, defaultTo: ''},
     data: {type: 'json', defaultTo: '{}'}
   }
 };
