73a63de762e578489752d8d406a7d2046ea74737
kmsheng <kmsh3ng@gmail.com>
2017-04-25 18:21:33 +0800

    update controller updateEntry

--- src/main/controllers/entry/updateEntry.js
+++ src/main/controllers/entry/updateEntry.js
@@ -4,10 +4,14 @@ export default async function updateEntry(event, rawData) {
   const {id, data} = rawData;
 
   const sourceEntry = data.sourceEntry;
+
+  delete data.entryId;
+  delete data.folderId;
   delete data.sourceEntry;
   delete data.id;
 
   await Entry.update({id}, {sourceEntry, data});
+  const entry = await Entry.findOne({id});
 
-  this.resolve({});
+  this.resolve(entry);
 }
