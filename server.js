347cbe59d01594ec7b1eb1a7e37b5d82d6468893
kmsheng <kmsh3ng@gmail.com>
2017-04-25 16:02:40 +0800

    disable hot reload

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
-  hot: true,
+  hot: false,
   historyApiFallback: true,
   stats: {
     colors: true
