112806a82362eb4bc374d5e17ea600ff280b175a
kmsheng <kmsh3ng@gmail.com>
2017-05-02 11:33:46 +0800

    re-style

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,14 @@ import {IndexRoute, Route} from 'react-router';
 
 import App from './containers/App/App';
 import PageAbout from './containers/PageAbout/PageAbout';
-import PageFolders from './containers/PageFolders/PageFolders';
-import PageSettings from './containers/PageSettings/PageSettings';
-import PageNotFound from './containers/PageNotFound/PageNotFound';
+import PageAddEntry from './containers/PageAddEntry/PageAddEntry';
+import PageEditEntry from './containers/PageEditEntry/PageEditEntry';
 import PageEditFolder from './containers/PageEditFolder/PageEditFolder';
 import PageEntries from './containers/PageEntries/PageEntries';
-import PageAddEntry from './containers/PageAddEntry/PageAddEntry';
 import PageEntry from './containers/PageEntry/PageEntry';
-import PageEditEntry from './containers/PageEditEntry/PageEditEntry';
+import PageFolders from './containers/PageFolders/PageFolders';
+import PageNotFound from './containers/PageNotFound/PageNotFound';
+import PageSettings from './containers/PageSettings/PageSettings';
 
 export default () => {
 
@@ -19,10 +19,12 @@ export default () => {
 
       <IndexRoute component={PageFolders} />
       <Route path="folders/:id/edit" component={PageEditFolder} />
+
       <Route path="folders/:id/entries" component={PageEntries} />
       <Route path="folders/:id/entries/add" component={PageAddEntry} />
       <Route path="folders/:folderId/entries/:entryId" component={PageEntry} />
       <Route path="folders/:folderId/entries/:entryId/edit" component={PageEditEntry} />
+
       <Route path="about" component={PageAbout} />
       <Route path="settings" component={PageSettings} />
       <Route path="*" component={PageNotFound} status={404} />
