6b2efd584c4bd39db0ca0c2bd64cf05e80af3177
kmsheng <kmsh3ng@gmail.com>
2017-05-03 17:40:05 +0800

    update controller addFolderByCsv

--- src/main/controllers/folder/addFolderByCsv.js
+++ src/main/controllers/folder/addFolderByCsv.js
@@ -2,9 +2,14 @@ import {dialog} from 'electron';
 import {isEmpty, first} from 'lodash';
 import csv from 'fast-csv';
 import fs from 'fs';
+import {basename} from 'path';
 
-export default async function addFolderByCsv({models}) {
+import csvProcessor from './../../helpers/csvProcessor';
 
+export default async function addFolderByCsv(args) {
+
+  const {models, webContents} = args;
+  const send = webContents.send.bind(webContents);
   const {Folder} = models;
 
   const options = {
@@ -14,25 +19,54 @@ export default async function addFolderByCsv({models}) {
     ]
   };
 
-  dialog.showOpenDialog(options, (paths) => {
+  dialog.showOpenDialog(options, handleDialogOpen);
+
+  async function handleDialogOpen(paths) {
 
     if (isEmpty(paths)) {
       return;
     }
 
+    send('start-processing-csv');
+
     const csvFilePath = first(paths);
+    const filename = basename(csvFilePath);
     const stream = fs.createReadStream(csvFilePath);
 
+    let completedLines = 0;
+    let hasColumnRow = false;
+    let folder = null;
+    let fields = [];
+
     const csvStream = csv()
-      .on('data', (data) => {
-        console.log(data);
+      .on('data', async (data) => {
+
+        if (isEmpty(folder) && csvProcessor.isColumnRow(data)) {
+          const columnData = csvProcessor.getColumnData(data);
+          folder = await Folder.create({
+            name: filename,
+            data: columnData
+          });
+          fields = csvProcessor.getFields(data);
+        }
+        else if (folder) {
+        }
+
+        if (0 === (++completedLines % 20000)) {
+          send('csv-processing-status', {completedLines});
+        }
       })
      .on('end', () => {
-       console.log("done");
+
+       if (! hasColumnRow) {
+         send('csv-processing-error', {message: 'No column row detected'});
+         return;
+       }
+
+       send('csv-processing-status', {completedLines});
+       send('csv-processing-end');
      });
 
     stream.pipe(csvStream);
-
-    console.log('csvFilePath', csvFilePath);
-  });
+  }
 }
