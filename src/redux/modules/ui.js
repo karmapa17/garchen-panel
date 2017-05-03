ec08db6ff79c7d52a90c59b4e1a9010c409a5f8c
kmsheng <kmsh3ng@gmail.com>
2017-05-03 17:38:52 +0800

    add setProcessingCsvStatus function

--- src/redux/modules/ui.js
+++ src/redux/modules/ui.js
@@ -4,10 +4,13 @@ import {createReducer} from 'redux-immutablejs';
 const SET_DRAWER_OPEN = 'garchen-panel/ui/SET_DRAWER_OPEN';
 const SET_SNACK_BAR_PARAMS = 'garchen-panel/ui/SET_SNACK_BAR_PARAMS';
 
+const SET_PROCESSING_CSV_STATUS = 'garchen-panel/ui/SET_PROCESSING_CSV_STATUS';
+
 const initialState = Map({
   isDrawerOpen: false,
   snackBarMessage: '',
-  isSnackBarOpen: false
+  isSnackBarOpen: false,
+  isProcessingCsv: true
 });
 
 export default createReducer(initialState, {
@@ -19,8 +22,19 @@ export default createReducer(initialState, {
   [SET_SNACK_BAR_PARAMS]: (state, action) => {
     return state.set('isSnackBarOpen', action.isSnackBarOpen)
       .set('snackBarMessage', action.snackBarMessage);
+  },
+
+  [SET_PROCESSING_CSV_STATUS]: (state, action) => {
+    return state.set('isProcessingCsv', action.isProcessingCsv);
   }
 });
+
+export function setProcessingCsvStatus(isProcessingCsv) {
+  return {
+    type: SET_PROCESSING_CSV_STATUS,
+    isProcessingCsv
+  };
+}
 
 export function setDrawerOpen(isDrawerOpen) {
   return {
