4b4b2558c8bc8e71c292ee54dd2f939a71c56b65
kmsheng <kmsh3ng@gmail.com>
2017-05-04 17:58:21 +0800

    update container PageImportCsv

--- src/containers/PageImportCsv/PageImportCsv.js
+++ src/containers/PageImportCsv/PageImportCsv.js
@@ -2,15 +2,32 @@ import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {injectIntl} from 'react-intl';
 import RaisedButton from 'material-ui/RaisedButton';
+import CircularProgress from 'material-ui/CircularProgress';
 
+import DICTIONARY_LANGS from './../../constants/dictionaryLangs';
 import injectF from './../../helpers/injectF';
 import ipc from './../../helpers/ipc';
-import {setProcessingCsvStatus} from './../../redux/modules/ui';
 import injectPush from './../../helpers/injectPush';
+import {addFolderByCsv} from './../../redux/modules/folder';
 
 const styles = require('./PageImportCsv.scss');
+const dictionaryLangs = DICTIONARY_LANGS.map((row) => row.value);
+const langExpression = '{lang}';
+const supportedColumns = [
+  {textId: 'source-entry', expression: 'source-entry-{lang}'},
+  {textId: 'explaination', expression: 'explaination-{lang}'},
+  {textId: 'explaination-note', expression: 'explaination-note-{lang}'},
+  {textId: 'page-num', expression: 'page-num'},
+];
 
-@connect(({}) => ({}), {setProcessingCsvStatus})
+const csvExampleUrl = 'https://goo.gl/YcRMrT';
+
+@connect(({folder}) => ({
+  isProcessingCsv: folder.get('isProcessingCsv'),
+  errorMessage: folder.get('errorCsvMessage'),
+  errorMessageId: folder.get('errorCsvMessageId'),
+  errorFilename: folder.get('errorCsvFilename')
+}), {addFolderByCsv})
 @injectIntl
 @injectPush
 @injectF
@@ -18,16 +35,19 @@ export default class PageImportCsv extends Component {
 
   static propTypes = {
     f: PropTypes.func.isRequired,
+    fh: PropTypes.func.isRequired,
     push: PropTypes.func.isRequired,
-    setProcessingCsvStatus: PropTypes.func.isRequired
+    isProcessingCsv: PropTypes.bool.isRequired,
+    errorMessage: PropTypes.string,
+    errorMessageId: PropTypes.string,
+    errorFilename: PropTypes.string,
+    addFolderByCsv: PropTypes.func.isRequired
   };
 
   constructor(props) {
     super(props);
     this.state = {
-      completedLines: 0,
-      errorMessage: '',
-      done: false
+      completedLines: 0
     };
   }
 
@@ -36,45 +56,73 @@ export default class PageImportCsv extends Component {
     this.setState({completedLines});
   };
 
-  handleCsvProcessingEnd = () => {
-    this.setState({done: true});
-  };
-
-  handleCsvProcessingError = (event, data) => {
-    this.setState({errorMessage: data.message});
-  };
-
   componentWillMount() {
     ipc.on('csv-processing-status', this.handleCsvProcessingStatus);
-    ipc.on('csv-processing-error', this.handleCsvProcessingError);
-    ipc.on('csv-processing-end', this.handleCsvProcessingEnd);
   }
 
   componentWillUnmount() {
     ipc.off('csv-processing-status', this.handleCsvProcessingStatus);
-    ipc.off('csv-processing-error', this.handleCsvProcessingError);
-    ipc.off('csv-processing-end', this.handleCsvProcessingEnd);
   }
 
   handleDoneButtonTouchTape = () => {
-    const {setProcessingCsvStatus} = this.props;
-    setProcessingCsvStatus(false);
   };
 
   handleConfirmButtonTouchTape = () => {
-    this.props.setProcessingCsvStatus(false);
+  };
+
+  handleExternalLinkButtonTouchTap = () => {
+  };
+
+  handleChooseCsvFileButtonTouchTap = () => {
+    this.props.addFolderByCsv();
+  }
+
+  renderChooseCsvFileButton = () => {
+
+    const {completedLines} = this.state;
+    const {f, isProcessingCsv} = this.props;
+
+    if (isProcessingCsv) {
+      return (
+        <div className={styles.chooseFileBtnWrap}>
+          {(completedLines > 0) && <div className={styles.linesCompleted}>{f('completed-csv-line', {completedLines: `${completedLines}`})}</div>}
+          <CircularProgress />
+        </div>
+      );
+    }
+    return (
+      <div className={styles.chooseFileBtnWrap}>
+        <RaisedButton label={f('choose-csv-file')} primary onTouchTap={this.handleChooseCsvFileButtonTouchTap} />
+      </div>
+    );
   };
 
   render() {
-    const {completedLines, done, errorMessage} = this.state;
-    const {f} = this.props;
+
+    const {completedLines} = this.state;
+    const {f, fh, errorMessage, errorMessageId, errorFilename} = this.props;
+    const supportedLangs = dictionaryLangs.map((lang) => `${f(lang)} ${lang}`)
+      .join(', ');
+
+    const supportedColumnNames = supportedColumns.map(({textId, expression}) => `${f(textId)} ${expression}`)
+      .join(', ');
+
+    const externalLinkButton = <a onTouchTap={this.handleExternalLinkButtonTouchTap}>{csvExampleUrl}</a>;
+
     return (
       <div className={styles.pageImportCsv}>
-        <h1>{f('import-csv-file')}</h1>
-        <h2>{f('completed-csv-line', {completedLines: `${completedLines}`})}</h2>
-        {errorMessage && <p>Error: {errorMessage}</p>}
-        {errorMessage && <RaisedButton label={f('confirm')} onTouchTap={this.handleConfirmButtonTouchTape} />}
-        {done && <RaisedButton label={f('see-imported-folder')} onTouchTap={this.handleDoneButtonTouchTape} />}
+        <div className={styles.content}>
+          <h1>{f('import-csv-file')}</h1>
+          <ul>
+            <li>{fh('import-csv-rule-1', {supportedLangs, langExpression})}</li>
+            <li>{fh('import-csv-rule-2', {langExpression})}</li>
+            <li>{fh('import-csv-rule-3', {supportedColumnNames})}</li>
+            <li>{fh('import-csv-rule-4')} {externalLinkButton}</li>
+          </ul>
+          {errorMessage && <div className={styles.errorMessage}>{errorMessage}</div>}
+          {errorMessageId && errorFilename && <div className={styles.errorMessage}>{f(errorMessageId, {errorFilename})}</div>}
+          {this.renderChooseCsvFileButton()}
+        </div>
       </div>
     );
   }
