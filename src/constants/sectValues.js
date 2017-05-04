3246fe7160a77ce6a886e59a4a3d954be75ee2a8
kmsheng <kmsh3ng@gmail.com>
2017-05-05 00:39:15 +0800

    update sect values format

--- src/constants/sectValues.js
+++ src/constants/sectValues.js
@@ -1,10 +1,3 @@
-export default [
-  {id: 'old', value: 'a'},
-  {id: 'when', value: 'b'},
-  {id: 'sa', value: 'c'},
-  {id: 'karma', value: 'd'},
-  {id: 'fragrant', value: 'e'},
-  {id: 'off', value: 'f'},
-  {id: 'time', value: 'g'},
-  {id: 'according', value: 'h'}
-];
+import {range} from 'ramda';
+
+export default range(1, 8).map((num) => ({id: `sect-${num}`, value: num}));
