173ff054bdd9413834157bf6a6bdfba2777bfd14
kmsheng <kmsh3ng@gmail.com>
2017-05-04 19:38:15 +0800

    update heading of PageAbout

--- src/containers/PageAbout/PageAbout.js
+++ src/containers/PageAbout/PageAbout.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 
+import injectF from './../../helpers/injectF';
+
 const styles = require('./PageAbout.scss');
 
+@injectF
 export default class PageAbout extends Component {
 
   render() {
     return (
       <div className={styles.pageAbout}>
-        <h1>About</h1>
+        <h2>{f('about')}</h2>
       </div>
     );
   }
