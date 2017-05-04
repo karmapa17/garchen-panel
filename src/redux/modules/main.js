85779f2a2a59175c61ed79860b6fdc6d5589a3c9
kmsheng <kmsh3ng@gmail.com>
2017-05-04 19:36:56 +0800

    add write delay related functions to main reducer

--- src/redux/modules/main.js
+++ src/redux/modules/main.js
@@ -3,20 +3,30 @@ import {createReducer} from 'redux-immutablejs';
 import {updateIntl} from 'react-intl-redux';
 
 import i18n from './../../helpers/i18n';
+import Cache from './../../helpers/Cache';
 
 const SET_APP_LOCALE = 'garchen-panel/main/SET_APP_LOCALE';
 const OPEN_EXTERNAL = 'garchen-panel/main/OPEN_EXTERNAL';
 const OPEN_EXTERNAL_SUCCESS = 'garchen-panel/main/OPEN_EXTERNAL_SUCCESS';
 const OPEN_EXTERNAL_FAIL = 'garchen-panel/main/OPEN_EXTERNAL_FAIL';
 
+const SET_WRITE_DELAY = 'garchen-panel/main/SET_WRITE_DELAY';
+
 const initialState = Map({
-  appLocale: i18n.getLocale()
+  appLocale: i18n.getLocale(),
+  writeDelay: Cache.get('garchen:writeDelay') || 50
 });
 
 export default createReducer(initialState, {
 
   [SET_APP_LOCALE]: (state, action) => {
     return state.set('appLocale', action.appLocale);
+  },
+
+  [SET_WRITE_DELAY]: (state, action) => {
+    const {writeDelay} = action;
+    Cache.set('garchen:writeDelay', writeDelay);
+    return state.set('writeDelay', writeDelay);
   }
 });
 
@@ -24,6 +34,13 @@ export function setAppLocale(appLocale) {
   return {
     type: SET_APP_LOCALE,
     appLocale
+  };
+}
+
+export function setWriteDelay(writeDelay) {
+  return {
+    type: SET_WRITE_DELAY,
+    writeDelay
   };
 }
 
