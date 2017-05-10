aeb357de6b714c4375352232d5317fe172bc58c8
kmsheng <kmsh3ng@gmail.com>
2017-05-10 11:05:13 +0800

    add search type

--- src/components/LocalSearchBar/LocalSearchBar.js
+++ src/components/LocalSearchBar/LocalSearchBar.js
@@ -1,5 +1,8 @@
 import React, {Component, PropTypes} from 'react';
 import {debounce} from 'lodash';
+import TextField from 'material-ui/TextField';
+import MenuItem from 'material-ui/MenuItem';
+import SelectField from 'material-ui/SelectField';
 
 import injectF from './../../helpers/injectF';
 
@@ -10,23 +13,44 @@ export default class LocalSearchBar extends Component {
 
   static propTypes = {
     f: PropTypes.func.isRequired,
-    onChange: PropTypes.func.isRequired
+    selectedSearchType: PropTypes.string,
+    searchTypes: PropTypes.array,
+    onInputChange: PropTypes.func.isRequired,
+    onSearchTypeChange: PropTypes.func
   };
 
-  debouncedChange = debounce((event) => {
-    this.props.onChange(event.target.value);
+  static defaultProps = {
+    searchTypes: []
+  };
+
+  debouncedInputChange = debounce((event) => {
+    this.props.onInputChange(event.target.value);
   }, 1000);
 
-  handleChange = (event) => {
+  handleInputChange = (event) => {
     event.persist();
-    this.debouncedChange(event);
+    this.debouncedInputChange(event);
   };
+
+  renderSearchTypes() {
+
+    const {f, searchTypes, selectedSearchType, onSearchTypeChange} = this.props;
+
+    if (0 === searchTypes.length) {
+      return false;
+    }
+    const items = searchTypes.map((type) => {
+      return <MenuItem value={type} key={`search-menu-item-${type}`} primaryText={f(type)} />;
+    });
+    return <SelectField value={selectedSearchType} onChange={onSearchTypeChange} style={{width: 160}}>{items}</SelectField>;
+  }
 
   render() {
     const {f} = this.props;
     return (
       <div className={styles.localSearchBar}>
-        <input className={styles.input} type="text" onChange={this.handleChange} placeholder={f('search-source-entry-or-page-num')} />
+        <TextField className={styles.searchInput} hintText={f('search-entries')} onChange={this.handleInputChange} />
+        {this.renderSearchTypes()}
       </div>
     );
   }
