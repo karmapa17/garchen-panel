40fcce68d16de457dd15d9dfe6691e9eb6a47b0b
kmsheng <kmsh3ng@gmail.com>
2017-05-11 18:25:17 +0800

    update container PageEntry

--- src/containers/PageEntry/PageEntry.js
+++ src/containers/PageEntry/PageEntry.js
@@ -14,8 +14,8 @@ import injectPush from './../../helpers/injectPush';
 import TopBar from './../../components/TopBar/TopBar';
 import Breadcrumb from './../../components/Breadcrumb/Breadcrumb';
 import EditEntryForm from './../../components/EditEntryForm/EditEntryForm';
-import EXPLAINATION_CATEGORY_VALUES from './../../constants/explainationCategoryValues';
 import filterLastContinuousUndefinedValues from './../../helpers/filterLastContinuousUndefinedValues';
+import renderContentFields from './renderContentFields';
 
 const styles = require('./PageEntry.scss');
 
@@ -56,91 +56,9 @@ export default class PageEntry extends Component {
   goToFoldersPage = () => this.props.push('/');
 
   renderContentFields() {
-
-    const {entry, f} = this.props;
-
-    const entryData = entry.data;
-    const keys = sortEntryKeys(Object.keys(entryData));
-    const rows = entryKeysToDataRows(keys, entryData, f);
-    const skipCols = ['explaination-source', 'explaination-note', 'explaination-category'];
-
-    return rows.map(({key, value, lang}) => {
-
-      if ('explaination' === key) {
-
-        const sourceArr = entryData[`explaination-source-${lang}`] || [];
-        const noteArr = entryData[`explaination-note-${lang}`] || [];
-        const categoryArr = entryData[`explaination-category-${lang}`] || [];
-
-        return value.map((v, index) => {
-
-          const nodes = [];
-
-          nodes.push((
-            <tr key={`${key}-${lang}-${index}`}>
-              <th>{f(`${key}-num-lang`, {lang: f(lang), num: (index + 1)})}</th>
-              <td>{v}</td>
-            </tr>
-          ));
-
-          const sourceValue = sourceArr[index];
-
-          if (sourceValue) {
-            nodes.push((
-              <tr key={`explaination-source-${lang}-${index}`}>
-                <th>{f(`explaination-source-num-lang`, {lang: f(lang), num: (index + 1)})}</th>
-                <td>{sourceValue}</td>
-              </tr>
-            ));
-          }
-
-          const noteValue = noteArr[index];
-
-          if (noteValue) {
-            nodes.push((
-              <tr key={`explaination-note-${lang}-${index}`}>
-                <th>{f(`explaination-note-num-lang`, {lang: f(lang), num: (index + 1)})}</th>
-                <td>{noteValue}</td>
-              </tr>
-            ));
-          }
-
-          const categoryValue = categoryArr[index];
-
-          if (categoryValue) {
-            const categoryId = EXPLAINATION_CATEGORY_VALUES.find((row) => row.value === categoryValue).id;
-            nodes.push((
-              <tr key={`explaination-category-${lang}-${index}`}>
-                <th>{f(`explaination-category-num-lang`, {lang: f(lang), num: (index + 1)})}</th>
-                <td>{f(categoryId)}</td>
-              </tr>
-            ));
-          }
-
-          return nodes;
-        });
-      }
-
-      if (skipCols.includes(key)) {
-        return false;
-      }
-
-      if (lang) {
-        return (
-          <tr key={`${key}-${lang}`}>
-            <th>{f(`${key}-lang`, {lang: f(lang)})}</th>
-            <td>{value}</td>
-          </tr>
-        );
-      }
-
-      return (
-        <tr key={key}>
-          <th>{f(key)}</th>
-          <td>{value}</td>
-        </tr>
-      );
-    });
+    const {entry: {data}, f, folder} = this.props;
+    const {targetLanguages, contentFields} = folder.data;
+    return renderContentFields({f, data, contentFields, targetLanguages});
   }
 
   goToEditPage = () => {
