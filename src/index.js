d52e23b35389850421eda37ede937ed37c13061e
kmsheng <kmsh3ng@gmail.com>
2017-05-13 03:49:33 +0800

    handle local storage stuff in src/index.js

--- src/index.js
+++ src/index.js
@@ -3,32 +3,40 @@ require('font-awesome-sass-loader!./../assets/styles/font-awesome-sass.config.js
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reducer from './redux/modules/reducer';
+import rootReducer from './redux/modules/reducer';
 import thunk from 'redux-thunk';
 import {Router, hashHistory} from 'react-router';
 import {IntlProvider} from 'react-intl-redux';
 import {Provider} from 'react-redux';
 import {addLocaleData} from 'react-intl';
 import clientMiddleware from './redux/middlewares/clientMiddleware';
-import {createStore, applyMiddleware} from 'redux';
+import {compose, createStore, applyMiddleware} from 'redux';
 import {syncHistoryWithStore} from 'react-router-redux';
+import persistState, {mergePersistedState} from 'redux-localstorage';
+import adapter from 'redux-localstorage/lib/adapters/localStorage';
 
 import bo from 'react-intl/locale-data/bo';
 import en from 'react-intl/locale-data/en';
 import zh from 'react-intl/locale-data/zh';
 
-
 import i18n from './helpers/i18n';
 import ipc from './helpers/ipc';
 import getRoutes from './routes';
+import serialize from './helpers/serialize';
+import deserialize from './helpers/deserialize';
 
 addLocaleData([...en, ...bo, ...zh]);
 
 const middlewares = [thunk, clientMiddleware(ipc)];
-const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
 const locale = i18n.getLocale();
 const localeData = i18n.getLocaleData(locale);
-const store = createStoreWithMiddleware(reducer, {intl: {locale, messages: localeData}});
+const reducer = compose(mergePersistedState(deserialize))(rootReducer);
+const storage = compose(serialize)(adapter(window.localStorage));
+const enhancer = compose(
+  applyMiddleware(...middlewares),
+  persistState(storage, 'garchen-redux-state')
+);
+const store = createStore(reducer, {intl: {locale, messages: localeData}}, enhancer);
 const history = syncHistoryWithStore(hashHistory, store);
 
 const routes = getRoutes(store);
