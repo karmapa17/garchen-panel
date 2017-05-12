9884366585d19fca2f2b51b66bb07aa70cbea189
kmsheng <kmsh3ng@gmail.com>
2017-05-12 18:52:51 +0800

    update controller getEntry ( select prev and next entry id )

--- src/main/controllers/entry/getEntry.js
+++ src/main/controllers/entry/getEntry.js
@@ -1,8 +1,49 @@
+import {get, isEmpty} from 'lodash';
+
+async function getNextEntryId({entry, db}) {
+
+  const query = db.knex('Entry')
+    .select('id')
+    .where('id', '>', entry.id)
+    .andWhere('folderId', entry.folderId)
+    .limit(1);
+
+  const res = await db.raw(query, true);
+  return get(res, '[0].id');
+}
+
+async function getPrevEntryId({entry, db}) {
+
+  const query = db.knex('Entry')
+    .select('id')
+    .where('id', '<', entry.id)
+    .andWhere('folderId', entry.folderId)
+    .orderBy('id', 'desc')
+    .limit(1);
+
+  const res = await db.raw(query, true);
+  return get(res, '[0].id');
+}
+
 export default async function getEntry(event, data) {
 
   const {id} = data;
-  const {Entry} = this.params.models;
+  const {db, models} = this.params;
+  const {Entry} = models;
+
   const entry = await Entry.findOne({id});
 
-  this.resolve(entry);
+  if (isEmpty(entry)) {
+    this.reject({message: 'entry not found'});
+    return;
+  }
+
+  const nextEntryId = await getNextEntryId({entry, db});
+  const prevEntryId = await getPrevEntryId({entry, db});
+
+  this.resolve({
+    entry,
+    nextEntryId,
+    prevEntryId
+  });
 }
