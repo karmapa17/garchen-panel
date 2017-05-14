78501f41642929b0765c2d14cd175b5358b82632
kmsheng <kmsh3ng@gmail.com>
2017-05-14 11:47:15 +0800

    add filtering props behavior

--- src/helpers/serialize.js
+++ src/helpers/serialize.js
@@ -1,6 +1,12 @@
+import getSubset from './getSubset';
+import toJson from './toJson';
+
 export default function serialize(storage) {
   return {
     ...storage,
-    put: (key, state, cb) => storage.put(key, state, cb)
+    put: (key, state, cb) => {
+      const data = getSubset(toJson(state), ['main.appLocale', 'main.writeDelay']);
+      return storage.put(key, data, cb);
+    }
   };
 }
