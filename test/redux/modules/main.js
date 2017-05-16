6f43c612091a980638a7119b1c5bf8fd0815e015
kmsheng <kmsh3ng@gmail.com>
2017-05-17 01:24:19 +0800

    test setInterfaceFontSizeScalingFactor

---
+++ test/redux/modules/main.js
@@ -0,0 +1,15 @@
+import test from 'ava';
+
+import {setInterfaceFontSizeScalingFactor, SET_INTERFACE_FONT_SIZE_SCALING_FACTOR,
+  setAppFont, SET_APP_FONT, setAppLocale, SET_APP_LOCALE} from './../../../src/redux/modules/main';
+
+test('should create an action to set interface font size scaling factor', (t) => {
+
+  const interfaceFontSizeScalingFactor = 1;
+  const expectedAction = {
+    type: SET_INTERFACE_FONT_SIZE_SCALING_FACTOR,
+    interfaceFontSizeScalingFactor
+  };
+
+  t.deepEqual(setInterfaceFontSizeScalingFactor(interfaceFontSizeScalingFactor), expectedAction);
+});
