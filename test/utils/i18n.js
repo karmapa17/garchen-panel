5b12c3b274cec73471dd6774cd87b6908edab292
kmsheng <kmsh3ng@gmail.com>
2017-05-17 00:31:44 +0800

    add test for helper i18n

---
+++ test/utils/i18n.js
@@ -0,0 +1,18 @@
+import test from 'ava';
+
+import i18n from './../../src/helpers/i18n';
+
+test('i18n.getLocale should work as expected', (t) => {
+  const result = i18n.getLocale();
+  t.is(result, 'en');
+});
+
+test('i18n.getLocaleData should work as expected for bo language code', (t) => {
+  const result = i18n.getLocaleData('bo');
+  t.is((Object.keys(result).length > 0), true);
+});
+
+test('i18n.getLocaleData should work as expected for other language code', (t) => {
+  const result = i18n.getLocaleData('en');
+  t.is((Object.keys(result).length > 0), true);
+});
