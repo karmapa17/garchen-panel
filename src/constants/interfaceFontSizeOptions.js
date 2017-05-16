f434d40b6ed7ff8fadad499ad24e8987334d1517
kmsheng <kmsh3ng@gmail.com>
2017-05-16 22:19:36 +0800

    add constant INTERFACE_FONT_SIZE_OPTIONS

---
+++ src/constants/interfaceFontSizeOptions.js
@@ -0,0 +1,7 @@
+export default [
+  {textId: 'very-small', value: 0.25},
+  {textId: 'small', value: 0.5},
+  {textId: 'medium', value: 1},
+  {textId: 'large', value: 1.25},
+  {textId: 'very-large', value: 1.5}
+];
