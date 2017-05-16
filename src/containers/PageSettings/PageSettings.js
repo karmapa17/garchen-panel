440a6098ddaa8b4dcee2cf715cac208b95f7e865
kmsheng <kmsh3ng@gmail.com>
2017-05-16 18:48:54 +0800

    add interface font size slider

--- src/containers/PageSettings/PageSettings.js
+++ src/containers/PageSettings/PageSettings.js
@@ -4,13 +4,14 @@ import MenuItem from 'material-ui/MenuItem';
 import LanguageIcon from 'material-ui/svg-icons/action/language';
 import TimerIcon from 'material-ui/svg-icons/image/timer';
 import TextFormatIcon from 'material-ui/svg-icons/content/text-format';
+import FormatSizeIcon from 'material-ui/svg-icons/editor/format-size';
 import Slider from 'material-ui/Slider';
 import {connect} from 'react-redux';
 import {injectIntl} from 'react-intl';
 
 import APP_LANGS from './../../constants/appLangs';
 import APP_FONTS from './../../constants/appFonts';
-import {setIntl, setAppFont, setWriteDelay} from './../../redux/modules/main';
+import {setIntl, setAppFont, setWriteDelay, setInterfaceFontSize} from './../../redux/modules/main';
 import injectF from './../../helpers/injectF';
 
 const styles = require('./PageSettings.scss');
@@ -18,8 +19,9 @@ const styles = require('./PageSettings.scss');
 @connect(({main}) => ({
   appLocale: main.get('appLocale'),
   appFont: main.get('appFont'),
+  interfaceFontSize: main.get('interfaceFontSize'),
   writeDelay: main.get('writeDelay')
-}), {setIntl, setWriteDelay, setAppFont})
+}), {setIntl, setWriteDelay, setAppFont, setInterfaceFontSize})
 @injectIntl
 @injectF
 export default class PageSettings extends Component {
@@ -27,8 +29,10 @@ export default class PageSettings extends Component {
   static propTypes = {
     f: PropTypes.func.isRequired,
     appLocale: PropTypes.string.isRequired,
+    interfaceFontSize: PropTypes.number.isRequired,
     appFont: PropTypes.string.isRequired,
     setIntl: PropTypes.func.isRequired,
+    setInterfaceFontSize: PropTypes.func.isRequired,
     setAppFont: PropTypes.func.isRequired,
     writeDelay: PropTypes.number.isRequired,
     setWriteDelay: PropTypes.func.isRequired
@@ -58,9 +62,13 @@ export default class PageSettings extends Component {
     this.props.setWriteDelay(newValue);
   };
 
+  handleInterfaceFontSizeSliderChange = (event, newValue) => {
+    this.props.setInterfaceFontSize(newValue);
+  };
+
   render() {
 
-    const {appLocale, appFont, f, writeDelay} = this.props;
+    const {appLocale, appFont, f, writeDelay, interfaceFontSize} = this.props;
 
     return (
       <div className={styles.pageSettings}>
@@ -71,6 +79,13 @@ export default class PageSettings extends Component {
             {this.renderLangMenuItems()}
           </SelectField>
         </div>
+        <div className={styles.customField}>
+          <TimerIcon style={{marginRight: '21px', marginBottom: '12px'}} />
+          <div className={styles.field}>
+            <span className={styles.label}>{f('write-delay-with-num', {writeDelay: `${writeDelay}`})}</span>
+            <Slider sliderStyle={{marginTop: '7px', marginBottom: 0}} step={1} style={{width: 240}} min={0} max={1000} defaultValue={writeDelay} onChange={this.handleWriteDelaySliderChange} />
+          </div>
+        </div>
         <div>
           <TextFormatIcon style={{marginRight: '21px', marginBottom: '12px'}} />
           <SelectField floatingLabelStyle={{fontSize: '20px'}} floatingLabelText={f('app-font')} onChange={this.handleFontSelectFieldChange} value={appFont}>
@@ -78,10 +93,10 @@ export default class PageSettings extends Component {
           </SelectField>
         </div>
         <div className={styles.customField}>
-          <TimerIcon style={{marginRight: '21px', marginBottom: '12px'}} />
+          <FormatSizeIcon style={{marginRight: '21px', marginBottom: '12px'}} />
           <div className={styles.field}>
-            <span className={styles.label}>{f('write-delay-with-num', {writeDelay: `${writeDelay}`})}</span>
-            <Slider sliderStyle={{marginTop: '7px', marginBottom: 0}} step={1} style={{width: 240}} min={0} max={1000} defaultValue={writeDelay} onChange={this.handleWriteDelaySliderChange} />
+            <span className={styles.label}>{f('interface-font-size', {interfaceFontSize})}</span>
+            <Slider sliderStyle={{marginTop: '7px', marginBottom: 0}} step={1} style={{width: 240}} min={12} max={48} defaultValue={interfaceFontSize} onChange={this.handleInterfaceFontSizeSliderChange} />
           </div>
         </div>
       </div>
