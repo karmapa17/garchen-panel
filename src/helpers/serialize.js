cb351f8e56dd18aedda6e5526d1287999066663f
kmsheng <kmsh3ng@gmail.com>
2017-05-16 14:19:04 +0800

    add main.appFont to be bached

--- src/helpers/serialize.js
+++ src/helpers/serialize.js
@@ -5,7 +5,7 @@ export default function serialize(storage) {
   return {
     ...storage,
     put: (key, state, cb) => {
-      const data = getSubset(toJson(state), ['main.appLocale', 'main.writeDelay']);
+      const data = getSubset(toJson(state), ['main.appLocale', 'main.appFont', 'main.writeDelay']);
       return storage.put(key, data, cb);
     }
   };
