059132c405f793b3d474264be6481f1d5e8056bf
kmsheng <kmsh3ng@gmail.com>
2017-05-16 23:57:50 +0800

    add test for helper/hasValue

---
+++ test/utils/hasValue.js
@@ -0,0 +1,27 @@
+import test from 'ava';
+
+import hasValue from './../../src/helpers/hasValue';
+
+test('hasValue should return false when null is present', (t) => {
+  const result = hasValue(null);
+  t.is(result, false);
+});
+
+test('hasValue should return false when undefined is present', (t) => {
+  const result = hasValue(undefined);
+  t.is(result, false);
+});
+
+test('hasValue should return false when empty string is present', (t) => {
+  const result = hasValue('');
+  t.is(result, false);
+});
+
+test('hasValue should work as expected', (t) => {
+  t.is(hasValue('true'), true);
+  t.is(hasValue(0), true);
+  t.is(hasValue(1.2), true);
+  t.is(hasValue({}), true);
+  t.is(hasValue([]), true);
+  t.is(hasValue(() => {}), true);
+});
