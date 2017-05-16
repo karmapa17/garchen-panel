bc258d6961e628aa34f506412c951c33a91387f5
kmsheng <kmsh3ng@gmail.com>
2017-05-17 00:41:37 +0800

    change test case wordings

--- test/utils/filterLastContinuousUndefinedValues.js
+++ test/utils/filterLastContinuousUndefinedValues.js
@@ -2,7 +2,7 @@ import test from 'ava';
 
 import filterLastContinuousUndefinedValues from './../../src/helpers/filterLastContinuousUndefinedValues';
 
-test('Cache should save my value', (t) => {
+test('filterLastContinuousUndefinedValues should drop last continuous null, undefined, or empty string values', (t) => {
   const result = filterLastContinuousUndefinedValues(['test', null, null, 'test', null, undefined, '']);
   t.deepEqual(result, ['test', null, null, 'test']);
 });
