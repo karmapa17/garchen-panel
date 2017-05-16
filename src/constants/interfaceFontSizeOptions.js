b98c633b6b88eefa1befd8394af1618886f48720
kmsheng <kmsh3ng@gmail.com>
2017-05-16 23:08:42 +0800

    change values of very-small and small option

--- src/constants/interfaceFontSizeOptions.js
+++ src/constants/interfaceFontSizeOptions.js
@@ -1,6 +1,6 @@
 export default [
-  {textId: 'very-small', value: 0.25},
-  {textId: 'small', value: 0.5},
+  {textId: 'very-small', value: 0.5},
+  {textId: 'small', value: 0.75},
   {textId: 'medium', value: 1},
   {textId: 'large', value: 1.25},
   {textId: 'very-large', value: 1.5}
