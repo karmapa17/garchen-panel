118c9cdf4e5ea9a1ce6af6729410086462437413
kmsheng <kmsh3ng@gmail.com>
2017-05-17 14:40:50 +0800

    add test for helpers/ipc

---
+++ test/utils/ipc.js
@@ -0,0 +1,73 @@
+import test from 'ava';
+
+import ipc from './../../src/helpers/ipc';
+
+const electron = window.require('electron');
+const {ipcRenderer, ipcMain} = electron;
+
+test('ipc should be able to send empty data and get fulfilled data back', async (t) => {
+
+  const eventName = 'ipc-send-empty-data-fulfill';
+
+  ipcMain.once(eventName, (event, args) => {
+    const id = args._id;
+    delete args._id;
+    event.sender.send(`${eventName}::${id}`, args);
+  });
+
+  t.deepEqual(await ipc.send(eventName), {});
+});
+
+test('ipc should be able to send data and get fulfilled data back', async (t) => {
+
+  const data = {testing: true};
+  const eventName = 'ipc-send-fulfill';
+
+  ipcMain.once(eventName, (event, args) => {
+    const id = args._id;
+    delete args._id;
+    event.sender.send(`${eventName}::${id}`, args);
+  });
+
+  t.deepEqual(await ipc.send(eventName, data), data);
+});
+
+test('ipc should be able to send data and get rejected error back', async (t) => {
+
+  const data = {testing: true};
+  const eventName = 'ipc-send-reject';
+
+  ipcMain.once(eventName, (event, args) => {
+    const id = args._id;
+    delete args._id;
+    args._error = true;
+    event.sender.send(`${eventName}::${id}`, args);
+  });
+
+  const error = await t.throws(ipc.send(eventName, data))
+  t.deepEqual(error, data);
+});
+
+test('ipc should be able to register event', async (t) => {
+
+  const eventName = 'ipc-on';
+  ipc.on(eventName, () => {});
+
+  const nameInEmitter = ipcRenderer._emitter.eventNames()
+    .filter((name) => name === eventName)[0];
+
+  t.is(eventName, nameInEmitter);
+});
+
+test('ipc should be able to de-register event', async (t) => {
+
+  const eventName = 'ipc-off';
+  const cb = () => {};
+  ipc.on(eventName, cb);
+  ipc.off(eventName, cb);
+
+  const nameInEmitter = ipcRenderer._emitter.eventNames()
+    .filter((name) => name === eventName)[0];
+
+  t.not(eventName, nameInEmitter);
+});
