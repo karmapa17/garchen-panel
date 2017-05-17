360863a42d94ad6271b231b7bf18fd036315433e
kmsheng <kmsh3ng@gmail.com>
2017-05-17 21:15:50 +0800

    add test for helpers/getSubset

---
+++ test/utils/getSubset.js
@@ -0,0 +1,41 @@
+import test from 'ava';
+
+import getSubset from './../../src/helpers/getSubset';
+
+test('getSubset should filter my paths', (t) => {
+
+  const subset = getSubset({
+    test1: true,
+    test2: {
+      test3: true,
+      test4: 0,
+      test5: false
+    },
+    test6: 'string',
+    test7: null
+  }, ['test2.test3', 'test2.test4', 'test2.test5', 'test6', 'test7']);
+
+  const expected = {
+    test2: {
+      test3: true,
+      test4: 0,
+      test5: false
+    },
+    test6: 'string'
+  };
+
+  t.deepEqual(subset, expected);
+});
+
+test('getSubset should work even if no paths were provided', (t) => {
+
+  const subset = getSubset({
+    test1: true,
+    test2: {
+      test3: true
+    },
+    test4: 'string'
+  });
+
+  t.deepEqual(subset, subset);
+});
