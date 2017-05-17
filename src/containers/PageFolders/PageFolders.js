15834850a2095e0b7d413e55c89035f12426970c
kmsheng <kmsh3ng@gmail.com>
2017-05-17 16:30:13 +0800

    fix typo

--- src/containers/PageFolders/PageFolders.js
+++ src/containers/PageFolders/PageFolders.js
@@ -140,12 +140,12 @@ export default class PageFolders extends Component {
         <Paper className={styles.folder} key={`paper-${id}`}>
           {isImporting && <LinearProgress mode="indeterminate" style={{marginBottom: '7px'}} />}
           <a className={styles.folderName} onTouchTap={this.handleFolderAnchorTouchTap(id)}>{name}</a>
-          {! isImporting && <IconMenu className={styles.folderIconMenu} style={{display: 'block', position: 'absolute'}}
+          {(! isImporting) && <IconMenu className={styles.folderIconMenu} style={{display: 'block', position: 'absolute'}}
             iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
             onChange={this.handleFolderMenuItemTouchTap}
             anchorOrigin={{horizontal: 'left', vertical: 'top'}}
             targetOrigin={{horizontal: 'left', vertical: 'top'}}>
-            <MenuItem primaryText={f('edit')} value={{type: 'edit', id}} />}
+            <MenuItem primaryText={f('edit')} value={{type: 'edit', id}} />
             <MenuItem primaryText={f('export')} value={{type: 'export', id}} />
           </IconMenu>}
         </Paper>
