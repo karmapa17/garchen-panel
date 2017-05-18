a381dd7f1650e66db34aa0b6ae535472365270bd
kmsheng <kmsh3ng@gmail.com>
2017-05-18 10:06:07 +0800

    add constant DEMO_FONT_PHRASES

---
+++ src/constants/demoFontPhrases.js
@@ -0,0 +1,6 @@
+export default [
+  {lang: 'en', value: 'The quick brown fox jump over the lazy dog'},
+  {lang: 'bo', value: 'བམ་པོ་དང་པོ། དཀོན་མཆོག་གསུམ་ལ་ཕྱག་འཚལ་ལོ། །གང་གིས་འཆིང་རྣམས་ཡང་'},
+  {lang: 'zh-TW', value: '遠古經典·明鏡新映'},
+  {lang: 'zh-CN', value: '远古经典·明镜新映'}
+];
