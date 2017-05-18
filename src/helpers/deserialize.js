9172f8f0948203354df5572b2f83920f92e091e1
kmsheng <kmsh3ng@gmail.com>
2017-05-19 00:40:54 +0800

    fix merge behavior

--- src/helpers/deserialize.js
+++ src/helpers/deserialize.js
@@ -4,7 +4,7 @@ export default function deserialize(state, payload) {
   return Object.entries(state)
     .reduce((obj, [prop, value]) => {
       const cachedValue = payload[prop];
-      obj[prop] = Map.isMap(value) ? value.merge(cachedValue) : Object.assign(value, cachedValue);
+      obj[prop] = Map.isMap(value) ? Map(value.merge(cachedValue).toJS()) : Object.assign(value, cachedValue);
       return obj;
     }, {});
 }
