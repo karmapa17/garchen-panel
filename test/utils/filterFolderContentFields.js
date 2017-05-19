e4d80f5eadc7b6a3cc01a8e0c64eb3de7a68f4e7
kmsheng <kmsh3ng@gmail.com>
2017-05-19 16:40:01 +0800

    add test for helper/filterFolderContentFields

---
+++ test/utils/filterFolderContentFields.js
@@ -0,0 +1,8 @@
+import test from 'ava';
+
+import filterFolderContentFields from './../../src/helpers/filterFolderContentFields';
+
+test('filterFolderContentFields should filter with given langs', (t) => {
+  const result = filterFolderContentFields(['en'], ['target-entry-en', 'target-entry-bo', 'explaination-lang-en', 'source']);
+  t.deepEqual(result, ["target-entry-en","target-entry-bo","explaination-lang-en", "source"]);
+});
