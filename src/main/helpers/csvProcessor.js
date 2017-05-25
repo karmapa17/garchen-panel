7cbb73c3e28f82e244bb3e891750c08540ed9a00
kmsheng <kmsh3ng@gmail.com>
2017-05-26 01:31:52 +0800

    add static function getCsvRowsByEntry to csvProcessor

--- src/main/helpers/csvProcessor.js
+++ src/main/helpers/csvProcessor.js
@@ -1,4 +1,4 @@
-import {uniq} from 'lodash';
+import {uniq, isEmpty} from 'lodash';
 
 import sortFolderContentFields from './sortFolderContentFields';
 import DICTIONARY_LANGS from './../constants/dictionaryLangs';
@@ -112,5 +112,60 @@ export default class CsvProcessor {
       }
       return oldData;
     }, oldData);
+  }
+
+  // for csv export
+  static getCsvRowsByEntry({folder, entry}) {
+
+    const {sourceLanguage, contentFields} = folder.data;
+
+    const defaultData = {
+      [`source-entry-${sourceLanguage}`]: entry.sourceEntry
+    };
+
+    let hasHandledExplainationNote = false;
+    let hasHandledExplainationSource = false;
+
+    return contentFields.reduce((rows, field) => {
+
+      if ('page-num' === field) {
+        rows[0]['page-num'] = entry.data['page-num'];
+      }
+
+      const [, explainationLang] = field.match(/^explaination-lang-(.+)$/) || [];
+
+      if (explainationLang) {
+        (entry.data[`explaination-${explainationLang}`] || []).forEach((explaination, index) => {
+          if (isEmpty(rows[index])) {
+            rows[index] = {};
+          }
+          rows[index][`explaination-${explainationLang}`] = explaination;
+        });
+
+        if (! hasHandledExplainationNote) {
+          const notes = entry.data['explaination-note'] || [];
+          notes.forEach((note, index) => {
+            if (isEmpty(rows[index])) {
+              rows[index] = {};
+            }
+            rows[index]['explaination-note'] = note;
+          });
+          hasHandledExplainationNote = true;
+        }
+
+        if (! hasHandledExplainationSource) {
+          const sources = entry.data['explaination-source'] || [];
+          sources.forEach((source, index) => {
+            if (isEmpty(rows[index])) {
+              rows[index] = {};
+            }
+            rows[index]['explaination-source'] = source;
+          });
+          hasHandledExplainationSource = true;
+        }
+      }
+
+      return rows;
+    }, [defaultData])
   }
 }
