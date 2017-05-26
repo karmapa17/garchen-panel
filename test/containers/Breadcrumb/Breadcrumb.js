c9fd82f1e033c6bab864a8b4a942ea1876c2abc7
kmsheng <kmsh3ng@gmail.com>
2017-05-26 16:21:41 +0800

    add test for connected Breadcrumb

--- test/containers/Breadcrumb/Breadcrumb.js
+++ test/containers/Breadcrumb/Breadcrumb.js
@@ -1,8 +1,12 @@
 import test from 'ava';
 import React from 'react';
 import {shallow, mount} from 'enzyme';
+import {Map} from 'immutable';
 
-import {Breadcrumb} from './../../../src/containers/Breadcrumb/Breadcrumb';
+import BreadcrumbConatiner, {Breadcrumb} from './../../../src/containers/Breadcrumb/Breadcrumb';
+import configureMockStore from 'redux-mock-store';
+
+const mockStore = configureMockStore([]);
 
 test('Breadcrumb should render properly', (t) => {
 
@@ -27,4 +31,23 @@ test('Breadcrumb.renderListContent should render single child', (t) => {
   const listContent = wrapper.instance().renderListContent();
 
   t.is(listContent.key, 'breadcrumb-item');
+});
+
+test('Breadcrumb container should render properly', (t) => {
+
+  const store = mockStore({
+    main: new Map({interfaceFontSizeScalingFactor: 1})
+  });
+
+  const wrapper = mount((
+    <BreadcrumbConatiner>
+      <a href="#link1">link1</a>
+    </BreadcrumbConatiner>
+  ), {
+    context: {
+      store
+    },
+  });
+
+  t.is(wrapper.find('ul').children('li').length, 1);
 });
