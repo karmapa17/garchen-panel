57d9a8a5ac2ae1075d65b400d761377de65a40f0
kmsheng <kmsh3ng@gmail.com>
2017-06-02 17:37:07 +0800

    reafactor test of main duck

--- test/redux/modules/main.js
+++ test/redux/modules/main.js
@@ -1,6 +1,7 @@
 import test from 'ava';
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
+import zhTwMessages from './../../../src/langs/zh-TW';
+import enMessages from './../../../src/langs/en';
+import mockStore from './../../helpers/mockStore';
 
 import mainReducer, {setInterfaceFontSizeScalingFactor, SET_INTERFACE_FONT_SIZE_SCALING_FACTOR,
   setAppFont, SET_APP_FONT, setAppLocale, SET_APP_LOCALE, setWriteDelay,
@@ -8,16 +9,14 @@ import mainReducer, {setInterfaceFontSizeScalingFactor, SET_INTERFACE_FONT_SIZE_
   setContentFontSizeScalingFactor, SET_CONTENT_FONT_SIZE_SCALING_FACTOR, getAppVersion,
   GET_APP_VERSION_SUCCESS} from './../../../src/redux/modules/main';
 
-import zhTwMessages from './../../../src/langs/zh-TW';
-import enMessages from './../../../src/langs/en';
-import clientMiddleware from './../../../src/redux/middlewares/clientMiddleware';
-import ipc from './../../../src/helpers/ipc';
-
-const middlewares = [thunk, clientMiddleware(ipc)];
-const mockStore = configureMockStore(middlewares);
-
 const electron = window.require('electron');
 const {ipcMain} = electron;
+
+let store;
+
+test.beforeEach((t) => {
+  store = mockStore({});
+});
 
 test('should create an action to set interface font size scaling factor', (t) => {
 
@@ -78,7 +77,6 @@ test('should create an action to set content font size scaling factor', (t) => {
 test('should get app version without any errors', async (t) => {
 
   const appVersion = '0.0.1';
-  const store = mockStore({});
   const expectedAction = {
     type: GET_APP_VERSION_SUCCESS
   };
@@ -98,7 +96,6 @@ test('should get app version without any errors', async (t) => {
 
 test('should create an action to set react intl', (t) => {
 
-  const store = mockStore({});
   const locale = 'zh-TW';
   const expectedAction = {
     type: '@@intl/UPDATE',
@@ -115,7 +112,6 @@ test('should create an action to set react intl', (t) => {
 
 test('should create an action to set react intl if no locale is provided', (t) => {
 
-  const store = mockStore({});
   const expectedAction = {
     type: '@@intl/UPDATE',
     payload: {
@@ -130,7 +126,6 @@ test('should create an action to set react intl if no locale is provided', (t) =
 });
 
 test('should open external url without any errors', (t) => {
-  const store = mockStore({});
   const url = 'https://www.google.com';
   store.dispatch(openExternal(url));
   t.pass();
@@ -138,42 +133,36 @@ test('should open external url without any errors', (t) => {
 
 test('main reducer should handle action GET_APP_VERSION_SUCCESS', (t) => {
   const appVersion = '0.0.1';
-  const store = mockStore({});
   const result = mainReducer(store.getState(), {type: GET_APP_VERSION_SUCCESS, result: {appVersion}});
   t.deepEqual(result.toJS(), {appVersion});
 });
 
 test('main reducer should handle action SET_APP_LOCALE', (t) => {
   const appLocale = 'en';
-  const store = mockStore({});
   const result = mainReducer(store.getState(), {type: SET_APP_LOCALE, appLocale});
   t.deepEqual(result.toJS(), {appLocale});
 });
 
 test('main reducer should handle action SET_WRITE_DELAY', (t) => {
   const writeDelay = 50;
-  const store = mockStore({});
   const result = mainReducer(store.getState(), {type: SET_WRITE_DELAY, writeDelay});
   t.deepEqual(result.toJS(), {writeDelay});
 });
 
 test('main reducer should handle action SET_APP_FONT', (t) => {
   const appFont = 'Tibetan Machine Uni';
-  const store = mockStore({});
   const result = mainReducer(store.getState(), {type: SET_APP_FONT, appFont});
   t.deepEqual(result.toJS(), {appFont});
 });
 
 test('main reducer should handle action SET_INTERFACE_FONT_SIZE_SCALING_FACTOR', (t) => {
   const interfaceFontSizeScalingFactor = 1.5;
-  const store = mockStore({});
   const result = mainReducer(store.getState(), {type: SET_INTERFACE_FONT_SIZE_SCALING_FACTOR, interfaceFontSizeScalingFactor});
   t.deepEqual(result.toJS(), {interfaceFontSizeScalingFactor});
 });
 
 test('main reducer should handle action SET_CONTENT_FONT_SIZE_SCALING_FACTOR', (t) => {
   const contentFontSizeScalingFactor = 1.5;
-  const store = mockStore({});
   const result = mainReducer(store.getState(), {type: SET_CONTENT_FONT_SIZE_SCALING_FACTOR, contentFontSizeScalingFactor});
   t.deepEqual(result.toJS(), {contentFontSizeScalingFactor});
 });
