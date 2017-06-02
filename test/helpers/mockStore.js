be78c5f704bb3d27be2913e5e8e64114c88a9014
kmsheng <kmsh3ng@gmail.com>
2017-06-02 17:15:05 +0800

    add helpers/mockStore.js

---
+++ test/helpers/mockStore.js
@@ -0,0 +1,8 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import clientMiddleware from './../../src/redux/middlewares/clientMiddleware';
+import ipc from './../../src/helpers/ipc';
+
+const middlewares = [thunk, clientMiddleware(ipc)];
+
+export default configureMockStore(middlewares);
