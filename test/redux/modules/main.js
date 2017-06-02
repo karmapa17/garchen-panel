827d4d5b90aa5d5f2f6a586d6439da983114addf
kmsheng <kmsh3ng@gmail.com>
2017-06-02 18:38:34 +0800

    sort functions

--- test/redux/modules/main.js
+++ test/redux/modules/main.js
@@ -18,6 +18,42 @@ test.beforeEach((t) => {
   store = mockStore({});
 });
 
+test('main reducer should handle action GET_APP_VERSION_SUCCESS', (t) => {
+  const appVersion = '0.0.1';
+  const result = mainReducer(store.getState(), {type: GET_APP_VERSION_SUCCESS, result: {appVersion}});
+  t.deepEqual(result.toJS(), {appVersion});
+});
+
+test('main reducer should handle action SET_APP_LOCALE', (t) => {
+  const appLocale = 'en';
+  const result = mainReducer(store.getState(), {type: SET_APP_LOCALE, appLocale});
+  t.deepEqual(result.toJS(), {appLocale});
+});
+
+test('main reducer should handle action SET_WRITE_DELAY', (t) => {
+  const writeDelay = 50;
+  const result = mainReducer(store.getState(), {type: SET_WRITE_DELAY, writeDelay});
+  t.deepEqual(result.toJS(), {writeDelay});
+});
+
+test('main reducer should handle action SET_APP_FONT', (t) => {
+  const appFont = 'Tibetan Machine Uni';
+  const result = mainReducer(store.getState(), {type: SET_APP_FONT, appFont});
+  t.deepEqual(result.toJS(), {appFont});
+});
+
+test('main reducer should handle action SET_INTERFACE_FONT_SIZE_SCALING_FACTOR', (t) => {
+  const interfaceFontSizeScalingFactor = 1.5;
+  const result = mainReducer(store.getState(), {type: SET_INTERFACE_FONT_SIZE_SCALING_FACTOR, interfaceFontSizeScalingFactor});
+  t.deepEqual(result.toJS(), {interfaceFontSizeScalingFactor});
+});
+
+test('main reducer should handle action SET_CONTENT_FONT_SIZE_SCALING_FACTOR', (t) => {
+  const contentFontSizeScalingFactor = 1.5;
+  const result = mainReducer(store.getState(), {type: SET_CONTENT_FONT_SIZE_SCALING_FACTOR, contentFontSizeScalingFactor});
+  t.deepEqual(result.toJS(), {contentFontSizeScalingFactor});
+});
+
 test('should create an action to set interface font size scaling factor', (t) => {
 
   const interfaceFontSizeScalingFactor = 1;
@@ -129,40 +165,4 @@ test('should open external url without any errors', (t) => {
   const url = 'https://www.google.com';
   store.dispatch(openExternal(url));
   t.pass();
-});
-
-test('main reducer should handle action GET_APP_VERSION_SUCCESS', (t) => {
-  const appVersion = '0.0.1';
-  const result = mainReducer(store.getState(), {type: GET_APP_VERSION_SUCCESS, result: {appVersion}});
-  t.deepEqual(result.toJS(), {appVersion});
-});
-
-test('main reducer should handle action SET_APP_LOCALE', (t) => {
-  const appLocale = 'en';
-  const result = mainReducer(store.getState(), {type: SET_APP_LOCALE, appLocale});
-  t.deepEqual(result.toJS(), {appLocale});
-});
-
-test('main reducer should handle action SET_WRITE_DELAY', (t) => {
-  const writeDelay = 50;
-  const result = mainReducer(store.getState(), {type: SET_WRITE_DELAY, writeDelay});
-  t.deepEqual(result.toJS(), {writeDelay});
-});
-
-test('main reducer should handle action SET_APP_FONT', (t) => {
-  const appFont = 'Tibetan Machine Uni';
-  const result = mainReducer(store.getState(), {type: SET_APP_FONT, appFont});
-  t.deepEqual(result.toJS(), {appFont});
-});
-
-test('main reducer should handle action SET_INTERFACE_FONT_SIZE_SCALING_FACTOR', (t) => {
-  const interfaceFontSizeScalingFactor = 1.5;
-  const result = mainReducer(store.getState(), {type: SET_INTERFACE_FONT_SIZE_SCALING_FACTOR, interfaceFontSizeScalingFactor});
-  t.deepEqual(result.toJS(), {interfaceFontSizeScalingFactor});
-});
-
-test('main reducer should handle action SET_CONTENT_FONT_SIZE_SCALING_FACTOR', (t) => {
-  const contentFontSizeScalingFactor = 1.5;
-  const result = mainReducer(store.getState(), {type: SET_CONTENT_FONT_SIZE_SCALING_FACTOR, contentFontSizeScalingFactor});
-  t.deepEqual(result.toJS(), {contentFontSizeScalingFactor});
 });
