c79e3c19961a408ad2f7bf3b2807b5ee2e2cd366
kmsheng <kmsh3ng@gmail.com>
2017-06-02 17:49:11 +0800

    remove store

--- test/redux/modules/ui.js
+++ test/redux/modules/ui.js
@@ -46,7 +46,6 @@ test('should create an action to set snack bar params ( empty snack bar message 
 
 test('ui reducer should handle action SET_DRAWER_OPEN', (t) => {
   const isDrawerOpen = true;
-  const store = mockStore({});
   const result = uiReducer(store.getState(), {type: SET_DRAWER_OPEN, isDrawerOpen});
   t.deepEqual(result.toJS(), {isDrawerOpen});
 });
@@ -54,7 +53,6 @@ test('ui reducer should handle action SET_DRAWER_OPEN', (t) => {
 test('ui reducer should handle action SET_SNACK_BAR_PARAMS', (t) => {
   const isSnackBarOpen = true;
   const snackBarMessage = 'hello world';
-  const store = mockStore({});
   const result = uiReducer(store.getState(), {type: SET_SNACK_BAR_PARAMS, isSnackBarOpen, snackBarMessage});
   t.deepEqual(result.toJS(), {isSnackBarOpen, snackBarMessage});
 });
