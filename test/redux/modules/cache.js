f98c7c7c3f5b7ee748d08c411f41658c58460695
kmsheng <kmsh3ng@gmail.com>
2017-06-02 17:15:19 +0800

    add test for cache duck

---
+++ test/redux/modules/cache.js
@@ -0,0 +1,34 @@
+import test from 'ava';
+import {Map} from 'immutable';
+import cacheReducer, {SET_CACHE_PAGE_ENTRIES, setCachePageEntries} from './../../../src/redux/modules/cache';
+import mockStore from './../../helpers/mockStore';
+
+let store;
+
+test.before((t) => {
+  store = mockStore(Map({
+    cachePageEntriesDataSet: {},
+    cachePageCrossFolderSearch: {},
+    cachePageFolders: {}
+  }));
+});
+
+test('should create an action to set cache page entries', (t) => {
+
+  const folderId = 1;
+  const data = {hello: 'world'};
+  const expectedAction = {
+    type: SET_CACHE_PAGE_ENTRIES,
+    folderId,
+    data
+  };
+
+  t.deepEqual(setCachePageEntries(folderId, data), expectedAction);
+});
+
+test('cache reducer should handle action SET_CACHE_PAGE_ENTRIES', (t) => {
+  const folderId = 1;
+  const data = {hello: 'world'};
+  const result = cacheReducer(store.getState(), {type: SET_CACHE_PAGE_ENTRIES, folderId, data});
+  t.deepEqual(result.toJS().cachePageEntriesDataSet, {[folderId]: data});
+});
