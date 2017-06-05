40d1d659ccc0cf51f25b989fc36d8eb7a1ebfa07
kmsheng <kmsh3ng@gmail.com>
2017-06-05 15:11:55 +0800

    add flag isDeleted

--- src/main/controllers/folder/listFolders.js
+++ src/main/controllers/folder/listFolders.js
@@ -1,9 +1,17 @@
 export default async function listFolders(event, data) {
 
-  const {page, perpage} = data;
+  const {page, perpage, isDeleted} = data;
   const {Folder} =  this.params.models;
+  let where;
 
-  const folders = await Folder.find({}, {skip: (page - 1) * perpage, limit: perpage});
+  if (isDeleted) {
+    where = ['deletedAt', '>', 0];
+  }
+  else {
+    where = {deletedAt: 0};
+  }
+
+  const folders = await Folder.find(null, where, {skip: (page - 1) * perpage, limit: perpage});
   const total = await Folder.count();
 
   this.resolve({data: folders, total});
