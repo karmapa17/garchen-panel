b32aabcaabf4113f0e9b219fba33454f11dad8ae
kmsheng <kmsh3ng@gmail.com>
2017-06-05 16:31:18 +0800

    re-style

--- src/main/controllers/folder/listFolders.js
+++ src/main/controllers/folder/listFolders.js
@@ -2,14 +2,7 @@ export default async function listFolders(event, data) {
 
   const {page, perpage, isDeleted} = data;
   const {Folder} =  this.params.models;
-  let where;
-
-  if (isDeleted) {
-    where = ['deletedAt', '>', 0];
-  }
-  else {
-    where = {deletedAt: 0};
-  }
+  const where = isDeleted ? ['deletedAt', '>', 0] : {deletedAt: 0};
 
   const folders = await Folder.find(null, where, {skip: (page - 1) * perpage, limit: perpage});
   const total = await Folder.count();
