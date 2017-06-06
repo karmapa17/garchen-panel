0d2517282845de594aac17beac8ca764beb592ae
kmsheng <kmsh3ng@gmail.com>
2017-06-06 10:53:08 +0800

    re-style

--- src/main/controllers/folder/markDeletedAtToFolders.js
+++ src/main/controllers/folder/markDeletedAtToFolders.js
@@ -4,7 +4,12 @@ export default async function markDeletedAtToFolders(event, data) {
   const {db, models} = this.params;
 
   const deletedAt = Math.round(+new Date() / 1000);
-  await db.raw(db.knex('Folder').whereIn('id', folderIds).update({deletedAt}));
+
+  const query = db.knex('Folder')
+    .whereIn('id', folderIds)
+    .update({deletedAt});
+
+  await db.raw(query);
 
   this.resolve({message: `Folders has been marked deletedAt successfully`});
 }
