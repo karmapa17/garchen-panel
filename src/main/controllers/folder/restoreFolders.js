8004d6e2dc904b1181411759b86b175c6debb997
kmsheng <kmsh3ng@gmail.com>
2017-06-06 10:59:50 +0800

    re-style

--- src/main/controllers/folder/restoreFolders.js
+++ src/main/controllers/folder/restoreFolders.js
@@ -2,7 +2,12 @@ export default async function restoreFolders(event, data) {
 
   const {folderIds} = data;
   const {db} = this.params;
-  await db.raw(db.knex('Folder').whereIn('id', folderIds).update({deletedAt: 0}));
+
+  const query = db.knex('Folder')
+    .whereIn('id', folderIds)
+    .update({deletedAt: 0});
+
+  await db.raw(query);
 
   this.resolve({message: `Folders have been restored successfully`});
 }
