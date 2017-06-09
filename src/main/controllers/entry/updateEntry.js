26b267e0455591ead07191ba8cbbdedca693099e
kmsheng <kmsh3ng@gmail.com>
2017-06-09 13:15:56 +0800

    use pageNum in controller updateEntry

--- src/main/controllers/entry/updateEntry.js
+++ src/main/controllers/entry/updateEntry.js
@@ -4,13 +4,15 @@ export default async function updateEntry(event, rawData) {
   const {id, data} = rawData;
 
   const sourceEntry = data.sourceEntry;
+  const pageNum = data.pageNum;
 
   delete data.entryId;
   delete data.folderId;
   delete data.sourceEntry;
   delete data.id;
+  delete data.pageNum;
 
-  await Entry.update({id}, {sourceEntry, data});
+  await Entry.update({id}, {sourceEntry, pageNum, data});
   const entry = await Entry.findOne({id});
 
   this.resolve(entry);
