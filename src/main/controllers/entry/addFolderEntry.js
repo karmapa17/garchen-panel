42f26835cbfa3ef63b79d749fea736f12f094324
kmsheng <kmsh3ng@gmail.com>
2017-06-09 13:15:27 +0800

    use pageNum in controller addFolderEntry

--- src/main/controllers/entry/addFolderEntry.js
+++ src/main/controllers/entry/addFolderEntry.js
@@ -5,11 +5,13 @@ export default async function addFolderEntry(event, rawData) {
   const {folderId, data} = rawData;
 
   const sourceEntry = data.sourceEntry;
+  const pageNum = data.pageNum;
 
   delete data.sourceEntry;
   delete data.folderId;
+  delete data.pageNum;
 
-  const entry = await Entry.create({folderId, sourceEntry, data});
+  const entry = await Entry.create({folderId, sourceEntry, pageNum, data});
 
   if (! entry) {
     return this.reject({message: 'Failed to create folder entry'});
