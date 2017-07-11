e140f2e5b576114b066826545412631f4467c899
chengchihhan <haneric1986@gmail.com>
2017-07-11 18:57:12 +0800

    add component PageJumper

---
+++ src/components/PageJumper/PageJumper.js
@@ -0,0 +1,41 @@
+import React, {Component, PropTypes} from 'react';
+
+import injectF from './../../helpers/injectF';
+
+@injectF
+export default class PageJumper extends Component {
+
+  static propTypes = {
+    f: PropTypes.func.isRequired,
+    current: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired,
+    onButtonTouchTap: PropTypes.func.isRequired
+  };
+
+  renderInputPage() {
+    const {current, total} = this.props;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="number" name="page" min="1" max={total}
+          ref={(input) => this.input = input} defaultValue={current} />
+        <span>&#47;{total}</span>
+      </form>
+    );
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const {current} = this.props;
+    const inputPageNum = parseInt(this.input.value.trim(), 10);
+    const equal = inputPageNum - current;
+    this.props.onButtonTouchTap(current + equal);
+  };
+
+  render() {
+    return (
+      <div>
+        {this.renderInputPage()}
+      </div>
+    );
+  }
+}
