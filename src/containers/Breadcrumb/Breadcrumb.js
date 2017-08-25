0e27620493f2a469e0dcc33ad765a9b68c497248
kmsheng <kmsh3ng@gmail.com>
2017-08-25 14:44:25 +0800

    remove font size settings in component Breadcrumb

--- src/containers/Breadcrumb/Breadcrumb.js
+++ src/containers/Breadcrumb/Breadcrumb.js
@@ -1,23 +1,15 @@
 import React, {Component, PropTypes} from 'react';
 import {isFunction} from 'lodash';
-import {connect} from 'react-redux';
-
-import getFontSize from './../../helpers/getFontSize';
 
 const styles = require('./Breadcrumb.scss');
 
-export class Breadcrumb extends Component {
+export default class Breadcrumb extends Component {
 
   static propTypes = {
     children: PropTypes.oneOfType([
       PropTypes.array.isRequired,
       PropTypes.object.isRequired
-    ]),
-    interfaceFontSizeScalingFactor: PropTypes.number.isRequired
-  };
-
-  static defaultProps = {
-    interfaceFontSizeScalingFactor: 1
+    ])
   };
 
   renderListContent() {
@@ -33,12 +25,6 @@ export class Breadcrumb extends Component {
   }
 
   render() {
-    const {interfaceFontSizeScalingFactor} = this.props;
-    const fontSize = getFontSize(interfaceFontSizeScalingFactor, 1.1);
-    return <ul style={{fontSize}} className={styles.breadcrumb}>{this.renderListContent()}</ul>;
+    return <ul className={styles.breadcrumb}>{this.renderListContent()}</ul>;
   }
 }
-
-export default connect(({main}) => ({
-  interfaceFontSizeScalingFactor: main.get('interfaceFontSizeScalingFactor')
-}))(Breadcrumb);
