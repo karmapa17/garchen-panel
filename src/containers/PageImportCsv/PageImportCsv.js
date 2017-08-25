360cb8bffaedb55d66884430be8292e64ff8d7d3
kmsheng <kmsh3ng@gmail.com>
2017-08-25 20:56:54 +0800

    remove linesPerSecond

--- src/containers/PageImportCsv/PageImportCsv.js
+++ src/containers/PageImportCsv/PageImportCsv.js
@@ -55,10 +55,7 @@ export default class PageImportCsv extends Component {
     addFolderByCsv: PropTypes.func.isRequired
   };
 
-  state = {
-    completedLines: 0,
-    linesPerSecond: 0
-  };
+  state = {completedLines: 0};
 
   handleCsvProcessingStatus = (event, data) => {
     this.setState({completedLines: data.completedLines, linesPerSecond: data.linesPerSecond});
