c6333e28ed1f0512bc6e98e75e66d97128a2a563
kmsheng <kmsh3ng@gmail.com>
2017-08-28 18:19:45 +0800

    upgrade db version to 0.0.11

--- src/main/models/index.js
+++ src/main/models/index.js
@@ -8,7 +8,7 @@ import {isArray} from 'lodash';
 import APP_DATA_PATH from './../constants/appDataPath';
 
 // if db version change, user will have to convert their db via online tool
-const dbVersion = '0.0.10';
+const dbVersion = '0.0.11';
 
 export default async function initDb() {
 
