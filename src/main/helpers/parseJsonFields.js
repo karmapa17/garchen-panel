8bd1c25fff3bee1a99ae788317bf3f678bce9246
kmsheng <kmsh3ng@gmail.com>
2017-08-31 16:01:05 +0800

    re-indent

--- src/main/helpers/parseJsonFields.js
+++ src/main/helpers/parseJsonFields.js
@@ -6,9 +6,9 @@ export default function parseJsonFields(rawFields, rows) {
   const fields = isArray(rawFields) ? rawFields : [rawFields];
 
   return rows.map((row) => {
-      return fields.reduce((row, field) => {
-        row[field] = safeJsonParse(row[field]);
-        return row;
-      }, Object.assign({}, row));
-    });
+    return fields.reduce((row, field) => {
+      row[field] = safeJsonParse(row[field]);
+      return row;
+    }, Object.assign({}, row));
+  });
 }
