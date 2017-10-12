6fc0a9806e7ec4c16e22eb595a3149c99712234e
kmsheng <kmsh3ng@gmail.com>
2017-10-12 20:36:37 +0800

    apply translation for about page

--- src/containers/PageAbout/PageAbout.js
+++ src/containers/PageAbout/PageAbout.js
@@ -1,26 +1,25 @@
 import React, {Component, PropTypes} from 'react';
-import {connect} from 'react-redux';
+import {FormattedHTMLMessage} from 'react-intl';
 
 import injectF from './../../utils/injectF';
 
 const styles = require('./PageAbout.scss');
 
-@connect(({main}) => ({
-  appVersion: main.get('appVersion')
-}))
 @injectF
 export default class PageAbout extends Component {
 
   static propTypes = {
     f: PropTypes.func.isRequired,
-    appVersion: PropTypes.string.isRequired
   };
 
   render() {
-    const {f, appVersion} = this.props;
+    const {f} = this.props;
     return (
       <div className={styles.pageAbout}>
-        <h2>{f('about-version', {appVersion})}</h2>
+        <h2>{f('about-heading')}</h2>
+        <div className={styles.aboutContent}>
+          <FormattedHTMLMessage id="about-content" />
+        </div>
       </div>
     );
   }
