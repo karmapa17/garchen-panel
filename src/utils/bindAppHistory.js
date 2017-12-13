ac2da0621fc9c350fec8dca9409c7df63edca275
kmsheng <kmsh3ng@gmail.com>
2017-12-13 13:52:18 +0800

    new file utils/bindAppHistory.js

---
+++ src/utils/bindAppHistory.js
@@ -0,0 +1,33 @@
+import React, {Component, PropTypes} from 'react';
+import {addRouteHistory, clearRouteHistory} from './../redux/modules/main';
+
+export default function bindAppHistory(Target) {
+
+  return class InjectedBindAppHistory extends Component {
+
+    static contextTypes = {
+      store: PropTypes.object.isRequired
+    };
+
+    static propTypes = {
+      push: PropTypes.func.isRequired,
+      location: PropTypes.object.isRequired
+    };
+
+    componentWillMount() {
+      this.context.store.dispatch(addRouteHistory(this.props.location.pathname));
+    }
+
+    render() {
+      const {push} = this.props;
+      const goBack = () => {
+        const {store} = this.context;
+        const routeHistory = store.getState().main.get('routeHistory');
+        const [firstRouteHistory] = routeHistory;
+        this.context.store.dispatch(clearRouteHistory());
+        push(firstRouteHistory);
+      };
+      return <Target {...this.props} goBack={goBack} />;
+    }
+  };
+}
